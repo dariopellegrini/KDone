9722fec446424f89401ec3d33c47fb55aca1e383
Dario Pellegrini <pellegrini.dario.1303@gmail.com>
2019-12-20 09:20:36 +0100

    App

--- src/main/kotlin/com/dariopellegrini/kdone/app.kt
+++ src/main/kotlin/com/dariopellegrini/kdone/app.kt
@@ -1,20 +1,4 @@
 package com.dariopellegrini.kdone
 
-import com.dariopellegrini.kdone.auth.*
-import com.dariopellegrini.kdone.configuration.Apple
-import com.dariopellegrini.kdone.configuration.Facebook
-import com.dariopellegrini.kdone.configuration.JWT
-import com.dariopellegrini.kdone.configuration.S3
-import com.dariopellegrini.kdone.model.DateModel
-import com.dariopellegrini.kdone.model.Identifiable
-import com.dariopellegrini.kdone.model.ResourceFile
-import com.dariopellegrini.kdone.routes.module
-import com.dariopellegrini.kdone.startup.startKDone
-import com.dariopellegrini.kdone.uploader.S3Uploader
-import com.dariopellegrini.kdone.user.model.KDoneUser
-import com.dariopellegrini.kdone.user.userModule
-import com.fasterxml.jackson.annotation.JsonInclude
-import java.util.*
-
 fun main() {
 }
