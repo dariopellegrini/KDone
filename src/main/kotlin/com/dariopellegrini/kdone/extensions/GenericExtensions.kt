fe4b685a475815f7ad80ed9beb2f77727ed5e4e6
Kama Sport <dev@kama.sport>
2020-04-06 20:50:35 +0200

    Added generic function to map from class instance to another class instance

--- src/main/kotlin/com/dariopellegrini/kdone/extensions/GenericExtensions.kt
+++ src/main/kotlin/com/dariopellegrini/kdone/extensions/GenericExtensions.kt
@@ -1,6 +1,7 @@
 package com.dariopellegrini.kdone.extensions
 
 import com.dariopellegrini.kdone.exceptions.ConstructionException
+import kotlin.reflect.KParameter
 import kotlin.reflect.full.memberProperties
 import kotlin.reflect.full.primaryConstructor
 
@@ -11,5 +12,18 @@ inline fun <reified I: Any, reified O: Any>I.map(): O {
         propertiesByName.containsKey(it.name) // Probably default parameter
     }.associateWith {
         propertiesByName[it.name]?.get(this@map)
+    })
+}
+
+inline fun <reified I: Any, reified O: Any>I.mapUsing(closure: (KParameter, I) -> Any): O {
+    val ctor = O::class.primaryConstructor ?: throw ConstructionException("Primary constructor for ${O::class} is missing")
+    val propertiesByName = I::class.memberProperties.associateBy { it.name }
+    return ctor. callBy(ctor.parameters.filter {
+        propertiesByName.containsKey(it.name) // Probably default parameter
+    }.associateWith {
+        when (val value = closure(it, this)) {
+            is Unit -> propertiesByName[it.name]?.get(this@mapUsing)
+            else -> value
+        }
     })
 }
