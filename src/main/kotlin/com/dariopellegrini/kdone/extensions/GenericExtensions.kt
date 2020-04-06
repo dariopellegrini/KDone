356e0e0c50a9d279d35c12e0e7c1595957165d27
Kama Sport <dev@kama.sport>
2020-04-06 22:29:00 +0200

    Added generic function to map from class instance to another class instance

--- src/main/kotlin/com/dariopellegrini/kdone/extensions/GenericExtensions.kt
+++ src/main/kotlin/com/dariopellegrini/kdone/extensions/GenericExtensions.kt
@@ -1,28 +1,47 @@
 package com.dariopellegrini.kdone.extensions
 
 import com.dariopellegrini.kdone.exceptions.ConstructionException
-import kotlin.reflect.KParameter
+import kotlin.reflect.KProperty1
 import kotlin.reflect.full.memberProperties
 import kotlin.reflect.full.primaryConstructor
 
-inline fun <reified I: Any, reified O: Any>I.map(): O {
+fun hello() {
+    val input = PersonInput("D", "P", "https://images.com/1.jpg")
+    val person: Person = input.remodelWith { parameter, value ->
+        when(parameter) {
+            PersonInput::image -> Image(value.image, "image/jpg")
+            else -> Unit
+        }
+    }
+    println(person)
+}
+
+data class Person(val id: String = "id1", val name: String, val surname: String, val image: Image)
+data class PersonInput(val name: String, val surname: String, val image: String)
+data class Image(val src: String, val mimeType: String)
+
+inline fun <reified I: Any, reified O: Any>I.remodel(): O {
     val constructor = O::class.primaryConstructor ?: throw ConstructionException("Primary constructor for ${O::class} is missing")
     val propertiesByName = I::class.memberProperties.associateBy { it.name }
     return constructor. callBy(constructor.parameters.filter {
         propertiesByName.containsKey(it.name) // Probably default parameter
     }.associateWith {
-        propertiesByName[it.name]?.get(this@map)
+        propertiesByName[it.name]?.get(this@remodel)
     })
 }
 
-inline fun <reified I: Any, reified O: Any>I.mapUsing(closure: (KParameter, I) -> Any): O {
+inline fun <reified I: Any, reified O: Any>I.remodelWith(closure: (KProperty1<I, *>, I) -> Any): O {
     val constructor = O::class.primaryConstructor ?: throw ConstructionException("Primary constructor for ${O::class} is missing")
     val propertiesByName = I::class.memberProperties.associateBy { it.name }
+    propertiesByName.map {
+        it.value
+    }
     return constructor.callBy(constructor.parameters.filter {
         propertiesByName.containsKey(it.name) // Probably default parameter
     }.associateWith {
-        when (val value = closure(it, this)) {
-            is Unit -> propertiesByName[it.name]?.get(this@mapUsing)
+        val property = propertiesByName[it.name] ?: return@associateWith null
+        when (val value = closure(property, this)) {
+            is Unit -> property.get(this@mapUsing)
             else -> value
         }
     })
