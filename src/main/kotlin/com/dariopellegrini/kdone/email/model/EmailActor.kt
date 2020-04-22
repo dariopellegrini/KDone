b6e89ff80f0c1f3bbb464f5d299a276e9955513b
Dario Pellegrini <dario.pellegrini@s4win.com>
2020-04-22 13:22:54 +0200

    Rename refactoring

--- src/main/kotlin/com/dariopellegrini/kdone/email/model/EmailActor.kt
+++
@@ -1,7 +0,0 @@
-package com.dariopellegrini.kdone.email.model
-
-data class EmailActor(val name: String, val address: String)
-
-fun sender(name: String, address: String): EmailActor {
-    return EmailActor(name, address)
-}
