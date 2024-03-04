18a61bc834590e434405cccf3d9f06fc6a0cdd76
Dario Pellegrini <dario.pellegrini@starktechnologypartner.com>
2024-03-04 16:55:30 +0100

    Added new object mapper extension

--- src/main/kotlin/com/dariopellegrini/kdone/extensions/ObjectMapperExtensions.kt
+++ src/main/kotlin/com/dariopellegrini/kdone/extensions/ObjectMapperExtensions.kt
@@ -2,13 +2,17 @@ package com.dariopellegrini.kdone.extensions
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.util.StdDateFormat
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.litote.kmongo.id.jackson.IdJacksonModule
 import java.text.SimpleDateFormat
+import java.util.*
 
 fun ObjectMapper.configureForKDone(): ObjectMapper {
     enable(SerializationFeature.INDENT_OUTPUT)
-    dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss'Z'")
+    val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    dateFormat.timeZone = TimeZone.getTimeZone("UTC")
+    this.dateFormat = dateFormat
     registerModule(IdJacksonModule())
     registerKotlinModule()
     return this
