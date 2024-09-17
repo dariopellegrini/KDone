af3ee9de5db071977332ae028fca5c3a67a10efb
Dario Pellegrini <dario.pellegrini@starktechnologypartner.com>
2024-09-17 16:02:07 +0200

    Added _id

--- src/main/kotlin/com/dariopellegrini/kdone/model/Identifiable.kt
+++ src/main/kotlin/com/dariopellegrini/kdone/model/Identifiable.kt
@@ -5,6 +5,6 @@ import org.litote.kmongo.Id
 import org.litote.kmongo.newId
 
 abstract class Identifiable {
-    open val _id: Id<Any> = newId()
+    open var _id: Id<Any> = newId()
     open var owner: Id<KDoneUser>? = null
 }
